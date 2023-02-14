0cb0892ab3ceacac018bab72d6accee8a632c48c
aldaces <shadarsi@gmail.com>
2023-02-14 11:33:28 +0200

    some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,8 @@ gulp.task('scripts', function() {
 		'app/libs/jquery/jquery-1.11.2.min.js',
 		'app/libs/waypoints/waypoints.min.js',
 		'app/libs/animate/animate-css.js',
+		'app/libs/slick/slick.min.js',
+		'app/libs/bootstrap/js/bootstrap..min.js',
 		])
 		.pipe(concat('libs.js'))
 		.pipe(uglify()) //Minify libs.js
